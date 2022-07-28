04e7e037069294d4bf78d3fd105c2f10aa150569
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-07-28 18:35:14 +0600

    client details add

---
+++ src/components/pages/Testimonials.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+const Testimonials = () => {
+
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+
+      
+    return (
+        <div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
---
+++ src/components/pages/clientsReview.js
@@ -0,0 +1,29 @@
+
+import client1 from '../../assets/images/client1.jpg'
+import client2 from '../../assets/images/client2.jpg'
+import client3 from '../../assets/images/client3.jpg'
+import client4 from '../../assets/images/client4.jpg'
+import client5 from '../../assets/images/client5.jpg'
+import client6 from '../../assets/images/client6.jpg'
+const clientsReview = () => {
+    const clientReview=[
+        {
+            name:'Robert',
+            img:client1,
+            feedBack:'Very useful in helping to reduce getting scammed by unscrupulous Amazon sellers. Blends in seamlessly with the website, easy to install and use!'
+        },
+        {
+            name:'Chris',
+            img:client2,
+            feedBack:`This is my go-to app whenever I purchase from Amazon. You can find out about the buyer instantly and even have the score rechecked. It's a big factor in whether or not I buy something.`
+        },
+        {
+            name:'Robert',
+            img:client3,
+            feedBack:'Very useful in helping to reduce getting scammed by unscrupulous Amazon sellers. Blends in seamlessly with the website, easy to install and use!'
+        },
+        
+    ]
+};
+
+export default clientsReview;
